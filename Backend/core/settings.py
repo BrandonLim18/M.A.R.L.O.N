5013aaaf378c41767be3acd50cab36944995f33e
Rockzxz <rockyaranez@gmail.com>
2026-05-22 20:58:48 +0800

    restore

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = '' 
+EMAIL_HOST_USER = 'marlonlibrarysystem@gmail.com'
+EMAIL_HOST_PASSWORD = 'pzxp jdre zdxh ykke' 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
